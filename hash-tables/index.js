6c1f38f3306387e42af73aeaca60975ed5567f2f
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-04-25 11:06:50 -0700

    setting up hashmap from scratch

--- hash-tables/index.js
+++ hash-tables/index.js
@@ -14,17 +14,41 @@ class HashTable {
 	set(key, val) {
 		const hashedKey = this._hash(key);
 		if (!this.data[hashedKey]) {
-			this.data[hashedKey] = [key, val];
+			this.data[hashedKey] = [];
 		}
+
+		this.data[hashedKey].push([key, val]);
+		return this.data;
 	}
+
 	get(key) {
 		const hashedKey = this._hash(key);
-		return this.data[hashedKey];
+		const currentBucket = this.data[hashedKey];
+
+		if (currentBucket) {
+			for (let i = 0; i < currentBucket.length; i++) {
+				if (currentBucket[i][0] == key) {
+					return currentBucket[i][1];
+				}
+			}
+		} else {
+			return undefined;
+		}
+	}
+	keys() {
+		const keysArray = [];
+		for (let i = 0; i < this.data.length; i++) {
+			if (this.data[i]) {
+				keysArray.push(this.data[i][0][0]);
+			}
+		}
+		return keysArray;
 	}
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-console.log(myHashTable.get("grapes"));
 myHashTable.set("apples", 9);
+console.log(myHashTable.get("grapes"));
 console.log(myHashTable.get("apples"));
+console.log(myHashTable.keys());
